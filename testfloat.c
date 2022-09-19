db86ff965fe63e26d7682476ca3475d7d53b37ce
SaptarishiD <96777857+SaptarishiD@users.noreply.github.com>
2022-09-19 23:43:08 +0530

    First commit

---
+++ testfloat.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char *str = "blah";
+    int count = 0;
+
+    for ( float x = 0.0; x < 0.0000000000000000000000000000000001; x = x + 0.00000000000000000000000000000000001 )
+
+    {
+
+    printf("%s ", str);
+    printf("%i\n", count);
+    count++;
+    
+
+    }
+
+
+}
