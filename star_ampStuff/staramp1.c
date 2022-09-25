a98b3eeb72b6b5b6cdbc8258223c85fc58d10c01
SaptarishiD <96777857+SaptarishiD@users.noreply.github.com>
2022-09-25 21:11:08 +0530

    First commit

--- star_ampStuff/staramp1.c
+++ star_ampStuff/staramp1.c
@@ -2,7 +2,16 @@
 
 int main(void)
 {
-    float n = 0.1 + 0.1 + 0.1;
-    printf("%f\n", n);
+    int* x;
+    int y = 4;
+    x = &y;
+    int* a;
+    int b = 5;
+    a = &b;
 
+    int* s = a - x; //incompatible integer to pointer conversion initializing 'int *' with an expression of type 'long
+                    // for + * and / was showing invalid operands to binary expression
+    printf("%i\n", a);
+    printf("%i\n", x);
+    printf("%i\n", s); //why is s always -4
 }
