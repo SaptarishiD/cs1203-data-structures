dd240b6580c63171e57f8c24bfd9ca0420e883e9
SaptarishiD <96777857+SaptarishiD@users.noreply.github.com>
2022-09-25 20:52:42 +0530

    First commit | Using *(a+1) notation

---
+++ mallocingStuff/malloc_array.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+int main(void)
+{
+    int* a;
+    char* b;
+    a = malloc(1000*sizeof(int));
+    *(a+1) = 42;
+    b = a;
+    printf("%d\n", *(a+1));
+    printf("%d\n", *(b+4));
+    
+
+    
+
+
+}
