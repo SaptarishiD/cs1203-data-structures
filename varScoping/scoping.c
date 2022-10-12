8a5b43bf98f49b4fbc2fd84e13d1a5da8c5fc7c3
SaptarishiD <96777857+SaptarishiD@users.noreply.github.com>
2022-10-12 17:46:21 +0530

    Error cause i is var local to foo but called in main

--- varScoping/scoping.c
+++ varScoping/scoping.c
@@ -2,7 +2,21 @@
 #include <stdlib.h>
 
 
+
+int foo(void);
+
 int main(void)
+{   
+    int x = foo();
+    int y = i+1;
+    printf("y is %d", y);
+}
+
+
+
+int foo(void)
 {
+    int i = 0;
+    return i;
     
 }
