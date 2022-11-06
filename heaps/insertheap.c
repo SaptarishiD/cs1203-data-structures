c9f0b36f329d3df91cf3147af5ec1c28a1674dea
SaptarishiD <96777857+SaptarishiD@users.noreply.github.com>
2022-11-07 00:27:53 +0530

    inserts element into minheap

--- heaps/insertheap.c
+++ heaps/insertheap.c
@@ -18,20 +18,22 @@ void swap(int * a, int * b)
     *b = temp;
 }
 
-void heapify(int arr[], int n, int i)
+void heapify(int arr[], int n, int nodeindex)
 {
 	// Find parent
-	int parent = (i - 1) / 2;
+	int parent = (nodeindex - 1) / 2;
 
 	if (arr[parent] > 0) 
     {
-		// For Max-Heap
-		// If current node is greater than its parent
+		// For min-Heap
+		// If current node is lesser than its parent
 		// Swap both of them and call heapify again
 		// for the parent
-		if (arr[i] > arr[parent]) 
+
+
+		if (arr[nodeindex] < arr[parent]) 
         {
-			swap(&arr[i], &arr[parent]);
+			swap(&arr[nodeindex], &arr[parent]);
 
 			// Recursively heapify the parent node
 			heapify(arr, n, parent);
@@ -40,13 +42,13 @@ void heapify(int arr[], int n, int i)
 }
 
 // Function to insert a new node to the Heap
-void insertNode(int arr[], int n, int key)
+void insertNode(int arr[], int n, int newelement)
 {
 	// Increase the size of Heap by 1
 	n = n + 1;
 
 	// Insert the element at end of Heap
-	arr[n - 1] = key;
+	arr[n - 1] = newelement;
 
 	// Heapify the new node following a
 	// Bottom-up approach
@@ -71,15 +73,27 @@ int main()
 	// 5 3
 	// / \
 	// 2 4
-	int arr[MAX] = { 10, 5, 3, 2, 4, 1 };
 
-	int n = 6;
+	int arr[MAX] = { 10, 5, 3, 2, 4 };
+
+    int arr1[MAX] = {0,11,22,33,44,55};
+
+	// int sizeofmaxarray = 5;
 
 	int insertelement = 15;
 
-	insertNode(arr, n, insertelement);
+    // int sizeofminarray = 6;
 
-	printArray(arr, n);
+	// insertNode(arr, sizeofmaxarray, insertelement);
+
+	// printArray(arr, sizeofmaxarray);
+
+    int sizeofminarray = 6;
+
+    insertNode(arr1, sizeofminarray, insertelement);
+
+	printArray(arr1, sizeofminarray);
+
 	// Final Heap will be:
 	// 15
 	// / \
