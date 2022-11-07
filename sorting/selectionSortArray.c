af5f5e7510dce30729023b0d74118e08085f3356
SaptarishiD <96777857+SaptarishiD@users.noreply.github.com>
2022-11-07 19:34:31 +0530

    User now inputs length of array and elements in array

--- sorting/selectionSortArray.c
+++ sorting/selectionSortArray.c
@@ -3,40 +3,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <ctype.h>
 
 
 
 void swap(int *a, int *b);
-int * generateArray(int n);
-void printArray(int * a, int n);
-void printArray1(int *a, int n);
-int findIndexOfMax(int *a, int n);
-void selectionSort(int *a, int n); 
+int * generateArray(int size);
+void printArray(int * array, int size);
+int findmaxindex(int *array, int size);
+void selectionSort(int *array, int size); 
 
 
 
 
 int main(int argc, char **argv) 
 {
-	int * a;
-	int n = 10;
-	nodeAddress list;
+	int * myarr;                            
+	int n;
+     
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    // error case in case there are no elements in the list
+	if(n < 1) 
+    {
+        printf("Array has no elements.\n");
+        return -1;
+    }
 
 	srand(time(NULL));
 
-	a = generateArray(n);
-	printArray(a,n);
-	selectionSort(a,n);
+	myarr = generateArray(n);
+	printArray(myarr,n);
+	selectionSort(myarr,n);
     printf("Sorted ");
-	printArray(a,n);
-
+	printArray(myarr,n);
 
 	return 0;
 }
 
 
 
-void swap(int * a, int * b)
+void swap(int * a, int * b) //swaps using addresses
 {
     int temp;
     temp = *a;
@@ -47,50 +55,51 @@ void swap(int * a, int * b)
 
 int * generateArray(int n) 
 {
-	int * t = malloc( n * sizeof(int) );
-	if(t) 
+	int * array = malloc( n * sizeof(int) );
+	if(array) 
     {
 		for(int i=0; i<n; i++) 
         {
-            t[i] = rand()%1000;
+            printf("Enter array element: ");
+            scanf("%d", &array[i]);
         }
 	}
-	return t;
+	return array;
 }
 
 
-void printArray(int * a, int n) 
+void printArray(int * array, int size)
 {
-	printf("Array = ");
-	for(int i=0; i<n; i++) 
+    printf("Array: ");
+  
+    for (int i = 0; i < size; i++)
     {
-		printf( i?", %d":"%d", a[i]);
-	}
-	printf(".\n");
+        
+        if (i == 0)
+        {
+            printf("%i", array[i]);
+        }
+        else
+        {
+            printf(", %i", array[i]);
+        }
+    }
+    printf("\n");
+    printf("\n");
+
 }
 
-void printArray1(int *a, int n) 
-{
-	for(int i=0; i<n; i++) 
-    {
-		printf(" %d ", a[i]);
-	}
-}
 
 
-
-int findIndexOfMax(int *a, int n) 
+int findmaxindex(int *array, int n) 
 {
 	int imax; // this will contain the address of the largest element encountered.
 	int i;
 
-	// error case if there are no elements in the list
-	if(n<1) {return -1;}
-
 	imax = 0; // I am assuming that a[0] is the max element
-	for(i=1; i<n; i++) 
+	for (i = 1; i < n; i++) 
     {
-		if( a[i] > a[imax] ) 
+		if( array[i] > array[imax] ) 
         {
 			imax = i;
 		}
@@ -99,14 +108,16 @@ int findIndexOfMax(int *a, int n)
 	return imax;
 }
 
-void selectionSort(int *a, int n) 
+void selectionSort(int *array, int n) 
 {
 	int i, imax, t;
 
-	for(i=n; i>1; i=i-1) 
+	for(i = n; i > 1; i = i-1) 
     {
-		imax = findIndexOfMax(a, i);
-		t = a[imax]; a[imax] = a[i-1]; a[i-1] = t;
+		imax = findmaxindex(array, i);
+		t = array[imax];
+        array[imax] = array[i-1];
+        array[i-1] = t;
 	}
 }
 
