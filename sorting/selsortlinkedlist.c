d0d63723ac800be3cceb07036d2d5f5d0038c799
SaptarishiD <96777857+SaptarishiD@users.noreply.github.com>
2022-11-07 22:46:42 +0530

    Added some comments and removed array sorting related code

--- sorting/selsortlinkedlist.c
+++ sorting/selsortlinkedlist.c
@@ -1,3 +1,6 @@
+//sorts linked list using selection sort by swapping the data at the nodes and not the nodes themselves
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -15,12 +18,10 @@ void swap(int *x, int *y);
 int * generateArray(int n);
 nodeAddress linkedListFromArray(int * a, int n);
 void freeLinkedList(nodeAddress head);
-
 void printArray(int * a, int n);
 void printLinkedList(nodeAddress head);
 int findIndexOfMax(int *a, int n);
 void selectionSort(int *a, int n);
-
 void selsortLL(nodeAddress head);
 void dataswap(nodeAddress min, nodeAddress temp);
 nodeAddress findMinValNode(nodeAddress forward, nodeAddress min);
@@ -35,10 +36,8 @@ int main(int argc, char **argv)
 {
 	int * a;
 	int n;
-    printf("Size of array: ");
+    printf("Enter size of array: ");
     scanf("%d", &n);
-	//int maxindex;
-	//int p, q;
 	nodeAddress list;
 
 	srand(time(NULL));
@@ -46,9 +45,6 @@ int main(int argc, char **argv)
 	a = generateArray(n);
 	list = linkedListFromArray(a,n);
 
-	printArray(a,n);
-	selectionSort(a,n);
-    printf("Sorted ");
 	printArray(a,n);
 
     printLinkedList(list);    
@@ -66,47 +62,46 @@ int main(int argc, char **argv)
 
 
 
-void swap(int *x, int *y) 
+void swap(int * a, int * b) //swaps using addresses
 {
-	int t;
-	t = *x;
-	*x = *y;
-	*y = t;
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;  
 }
-
 
 int * generateArray(int n) 
 {
-	int * t = malloc( n * sizeof(int) );
-	if(t) 
+	int * array = malloc( n * sizeof(int) );
+	if(array) 
     {
 		for(int i=0; i<n; i++) 
         {
-            t[i] = rand()%1000;
+            printf("Enter array element: ");
+            scanf("%d", &array[i]);
         }
 	}
-	return t;
+	return array;
 }
 
-nodeAddress linkedListFromArray(int * a, int n) 
+nodeAddress linkedListFromArray(int * a, int n) // generates linked list from array with the data in the same order as the array
 {
 	int i;
 	nodeAddress head = NULL;
 	nodeAddress temp = NULL;
 
-	// special case for head
-	if(n>0) 
+	if(n > 0) 
     {
-		head = malloc( sizeof(struct node) );
+		head = malloc(sizeof(struct node)); //special case for head
 		head->val  = a[0];
 		head->next = NULL;
 		temp = head;
 	}
 
 
-	for(i=1; i<n; i++) 
+	for(i = 1; i < n; i++) 
     {
-		temp->next = malloc( sizeof(struct node) );
+		temp->next = malloc(sizeof(struct node));
 		temp = temp->next;
 		temp->val = a[i];
 		temp->next = NULL;
@@ -131,7 +126,7 @@ void freeLinkedList(nodeAddress head)
 void printArray(int *a, int n) 
 {
     printf("Array = ");
-	for(int i=0; i<n; i++) 
+	for(int i = 0; i < n; i++) 
     {
 		if (i == 0)
         {
@@ -159,43 +154,7 @@ void printLinkedList(nodeAddress head)
 }
 
 
-int findIndexOfMax(int *a, int n) 
-{
-	int imax; // this will contain the address of the largest element encountered.
-	int i;
-
-	// error case if there are no elements in the list
-	if(n<1) {return -1;}
-
-	imax = 0; // I am assuming that a[0] is the max element
-	for(i=1; i<n; i++) 
-    {
-		if( a[i] > a[imax] ) 
-        {
-			imax = i;
-		}
-	}
-
-	return imax;
-}
-
-void selectionSort(int *a, int n) 
-{
-	int i, imax, t;
-
-	for(i=n; i>1; i=i-1) 
-    {
-		imax = findIndexOfMax(a, i);
-		t = a[imax]; 
-        a[imax] = a[i-1];
-        a[i-1] = t;
-	}
-}
-
-
-
-
-void dataswap(nodeAddress min, nodeAddress temp)
+void dataswap(nodeAddress min, nodeAddress temp) //swaps data between nodes
 {
     int tempint = min->val;
     min->val = temp->val;
@@ -207,7 +166,7 @@ void dataswap(nodeAddress min, nodeAddress temp)
 
 nodeAddress findMinValNode(nodeAddress forward, nodeAddress min)
 {
-    while(forward)                      // finds the minimum element
+    while(forward)                      // finds the minimum element in the linked list and returns the respective node
         {
             if (forward->val < min->val)
             {
@@ -222,10 +181,8 @@ nodeAddress findMinValNode(nodeAddress forward, nodeAddress min)
 
 void selsortLL(nodeAddress head)
 {
-    //do the max version
-    
+   
     nodeAddress temp = head;
-
 
     while (temp)
     {
@@ -233,13 +190,6 @@ void selsortLL(nodeAddress head)
         nodeAddress forward = temp->next;
 
         min = findMinValNode(forward, min);
-
-        // 5->4->2->3->1
-        // at end of while loop min points to 1 so the min element is found
-        
-        //==========================================
-        //this is data swapping one
-
         int tempint = min->val;
         min->val = temp->val;
         temp->val = tempint;
