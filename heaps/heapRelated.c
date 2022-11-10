8396967249f4a14afc1e101a6668412bcd1cc2f1
SaptarishiD <96777857+SaptarishiD@users.noreply.github.com>
2022-11-11 01:17:44 +0530

    Generates heap from array taking into consideration equal elements

--- heaps/heapRelated.c
+++ heaps/heapRelated.c
@@ -7,7 +7,8 @@
 #define arraysize 500
 
 void swap(int * a, int * b);
-int * generateArray(int size);
+int * generateArray1(int size);
+int * generateArray(int size); 
 void print_array(int * array, int size);
 int * build_heap(int * array, int size);
 void sift_down(int * array, int i);
@@ -21,9 +22,9 @@ void sift_down(int * array, int i);
 int main(void)
 {
     srand(1000);
-    int n = 10;
-    //printf("Enter size of array: ");
-    //scanf("%d", &n);
+    int n;
+    printf("Enter size of array: ");
+    scanf("%d", &n);
     int * array = generateArray(n);
     printf("Random ");
     print_array(array, n);
@@ -45,12 +46,12 @@ int main(void)
 }
 
 
-int * generateArray(int size) 
+int * generateArray1(int size) 
 {
 	int * array = malloc( size * sizeof(int) );
 	if(array) 
     {
-        for (int i = 0; i < size; i++)
+        for (int i = 0; i <= size; i++)
         {
             array[0] = 15;
             array[1] = 10;
@@ -59,7 +60,7 @@ int * generateArray(int size)
             array[4] = 5;
             array[5] = 20;
             array[6] = 4;
-            array[7] = 7;
+            array[7] = 19;
             array[8] = 6;
             array[9] = 21;
         }       
@@ -68,13 +69,28 @@ int * generateArray(int size)
 	return array;
 }
 
+int * generateArray(int size) 
+{
+	int * array = malloc( size * sizeof(int) );
+	if(array) 
+    {
+        for (int i = 0; i <= size; i++)
+        {
+            array[i] = rand()%25;
+        }       
+     
+	}
+	return array;
+}
+
+
 
 
 void print_array(int * array, int size)
 {
     printf("Array: ");
   
-    for (int i = 0; i < size; i++)
+    for (int i = 0; i <= size; i++)
     {
         
         if (i == 0)
@@ -103,32 +119,31 @@ void swap(int * a, int * b)
 int * build_heap(int * array, int size)
 {
     int startindex = (size - 2)/2; // going to the first problem parent since last row is assumed to be heap
-    assert(startindex == 4);
     int i = startindex;
 
     while(i >= 0)
     {
         if (array[2*i + 1] && array[2*i + 2])   // if both left and right child exist
         {
-            if (array[i] < array[2*i + 1] && array[i] < array[2*i + 2])
+            if (array[i] <= array[2*i + 1] && array[i] <= array[2*i + 2])
             {
                 i--;
             }
-            else if (array[i] > array[2*i + 1] && array[i] < array[2*i + 2])
+            else if (array[i] >= array[2*i + 1] && array[i] <= array[2*i + 2])
             {
                 swap(&array[i], &array[2*i + 1]);
                 sift_down(array, 2*i + 1);
                 i--;
             }
-            else if (array[i] < array[2*i + 1] && array[i] > array[2*i + 2])
+            else if (array[i] <= array[2*i + 1] && array[i] >= array[2*i + 2])
             {
                 swap(&array[i], &array[2*i + 2]);
                 sift_down(array, 2*i + 2);
                 i--;
             }
-            else if (array[i] > array[2*i + 1] && array[i] > array[2*i + 2])
+            else if (array[i] >= array[2*i + 1] && array[i] >= array[2*i + 2])
             {
-                if ( array[2*i + 1] <  array[2*i + 2])
+                if ( array[2*i + 1] <=  array[2*i + 2])
                 {
                     swap(&array[i], &array[2*i +1]);
                     sift_down(array, 2*i + 1);
@@ -136,45 +151,49 @@ int * build_heap(int * array, int size)
 
                 }
 
-                else if ( array[2*i + 1] >  array[2*i + 2])
+                else if ( array[2*i + 1] >=  array[2*i + 2])
                 {
                     swap(&array[i], &array[2*i +2]);
                     sift_down(array, 2*i + 2);
                     i--;
 
                 }
+                // else{i--;}     
             }
         }
 
 
         else if (array[2*i + 1] && !array[2*i + 2]) // if only left child exists
         {
-            if (array[i] < array[2*i + 1])
+            if (array[i] <= array[2*i + 1])
             {
                 i--;
             }
-            else if (array[i] > array[2*i + 1])
+            else if (array[i] >= array[2*i + 1])
             {
                 swap(&array[i], &array[2*i + 1]);
                 sift_down(array, 2*i + 1);
                 i--;
-            }           
+            }
+            // else{i--;}           
         }
 
 
         else if (!array[2*i + 1] && array[2*i + 2]) //if only right child exists
         {
-            if (array[i] < array[2*i + 2])
+            if (array[i] <= array[2*i + 2])
             {
                 i--;
             }
-            else if (array[i] > array[2*i + 2])
+            else if (array[i] >= array[2*i + 2])
             {
                 swap(&array[i], &array[2*i + 2]);
                 sift_down(array, 2*i + 2);
                 i--;
-            }           
+            } 
+            // else{i--;}          
         }
+        // else{i--;}
         
         
 
@@ -193,64 +212,69 @@ void sift_down(int * array, int i)
 {
     if (array[2*i + 1] && array[2*i + 2])
     {
-        if (array[i] < array[2*i + 1] && array[i] < array[2*i + 2])
+        if (array[i] <= array[2*i + 1] && array[i] <= array[2*i + 2])
         {
             return;
         }
-        else if (array[i] > array[2*i + 1] && array[i] < array[2*i + 2])
+        else if (array[i] >= array[2*i + 1] && array[i] <= array[2*i + 2])
         {
             swap(&array[i], &array[2*i + 1]);
             sift_down(array, 2*i + 1);
         }
-        else if (array[i] < array[2*i + 1] && array[i] > array[2*i + 2])
+        else if (array[i] <= array[2*i + 1] && array[i] >= array[2*i + 2])
         {
             swap(&array[i], &array[2*i + 2]);
             sift_down(array, 2*i + 2);
         }
-        else if (array[i] > array[2*i + 1] && array[i] > array[2*i + 2])
+        else if (array[i] >= array[2*i + 1] && array[i] >= array[2*i + 2])
         {
-            if ( array[2*i + 1] <  array[2*i + 2])
+            if ( array[2*i + 1] <=  array[2*i + 2])
             {
                 swap(&array[i], &array[2*i +1]);
                 sift_down(array, 2*i + 1);
             }
 
-            else if ( array[2*i + 1] >  array[2*i + 2])
+            else if ( array[2*i + 1] >=  array[2*i + 2])
             {
                 swap(&array[i], &array[2*i +2]);
                 sift_down(array, 2*i + 2);
             }
+            // else{return;}
         } 
+        // else{return;}
     }
 
 
     else if (array[2*i + 1] && !array[2*i + 2]) // if only left child exists
     {
-        if (array[i] < array[2*i + 1])
+        if (array[i] <= array[2*i + 1])
         {
             return;
         }
-        else if (array[i] > array[2*i + 1])
+        else if (array[i] >= array[2*i + 1])
         {
             swap(&array[i], &array[2*i + 1]);
             sift_down(array, 2*i + 1);
             
-        }           
+        }
+        // else{return;}           
     }
 
 
     else if (!array[2*i + 1] && array[2*i + 2]) //if only right child exists
     {
-        if (array[i] < array[2*i + 2])
+        if (array[i] <= array[2*i + 2])
         {
             return;
         }
-        else if (array[i] > array[2*i + 2])
+        else if (array[i] >= array[2*i + 2])
         {
             swap(&array[i], &array[2*i + 2]);
             sift_down(array, 2*i + 2);
             
-        }           
+        }
+        // else{return;}           
     }
+    // else{return;}
 
 }
