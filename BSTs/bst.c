03dc3f4198afdf5809c292070eedc838901f38e0
SaptarishiD <96777857+SaptarishiD@users.noreply.github.com>
2022-11-14 00:49:09 +0530

    deletes particular node in the tree

--- BSTs/bst.c
+++ BSTs/bst.c
@@ -15,6 +15,11 @@ int * makearray(int size);
 nodeaddress createBST(int * array, int size);
 nodeaddress createNode(int data);
 nodeaddress insert(nodeaddress root, int data);
+nodeaddress delete(nodeaddress root, int nodedata);
+int findMinRightSubtree(nodeaddress root);
+void freenode(nodeaddress root);
+
+
 
 
 void print2DUtil(nodeaddress root, int space);
@@ -28,19 +33,17 @@ void print2D(nodeaddress root);
 
 int main(void)
 {
-    int n = 10;
+    int n = 7;
     int * array = makearray(n);
 
     nodeaddress bstroot = createBST(array, n);
-    
-    
     print2D(bstroot);
+    nodeaddress newroot = delete(bstroot, 18);
+    print2D(newroot);
 
 
-
-
-
-
+    free(array);
+    freenode(bstroot);
 
 
 
@@ -57,15 +60,15 @@ int * makearray(int size)
         for (int i = 0; i < size; i++)
         {
             array[0] = 15;
-            array[1] = 10;
-            array[2] = 2;
-            array[3] = 19;
-            array[4] = 5;
-            array[5] = 20;
-            array[6] = 4;
-            array[7] = 19;
-            array[8] = 6;
-            array[9] = 0;
+            array[1] = 3;
+            array[2] = 17;
+            array[3] = 1;
+            array[4] = 2;
+            array[5] = 16;
+            array[6] = 18;
+            // array[7] = 18;
+            // array[8] = 6;
+            // array[9] = 0;
         }       
      
 	}
@@ -117,7 +120,114 @@ nodeaddress insert(nodeaddress root, int data)
 }
 
 
+nodeaddress search(nodeaddress root, int data) 
+{
+	if(root==NULL) return NULL;
+
+	if (data < root->val) 
+	{ 
+        return search(root->left , data);
+    }
+	else if (data > root->val) 
+	{ 
+        return search(root->right, data);
+    }
+	else                        
+	{ 
+        return root;
+    }
+}
+
+
+nodeaddress delete(nodeaddress root, int nodedata)
+{
+    nodeaddress node = search(root, 17);
+
+    if (node->left == NULL && node->right == NULL)
+    {
+        free(node);
+        return NULL;
+    }
+
+    else if(node->left == NULL && node->right != NULL)
+    {
+        nodeaddress temp = node->right;
+        free(node);
+        return temp;
+    }
+
+    else if(node->left != NULL && node->right == NULL)
+    {
+        nodeaddress temp = node->left;
+        free(node);
+        return temp;
+    }
+
+    else if (node->left != NULL && node->right != NULL)
+    {
+        int min = findMinRightSubtree(node->right);
+        node->val = min;
+        node->right = delete(node->right, min);     // need to actually free the node associate with min
+    }
+
+
+    return root;
+
+
+}
+
+
+int findMinRightSubtree(nodeaddress root)
+{
+    nodeaddress temp = root;
+
+    while(temp->left !=NULL)
+    {
+        temp = temp->left;
+    }
+
+    return temp->val;
+
+}
+
+
+void freenode(nodeaddress root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    else if(root->left == NULL && root->right != NULL)
+    {
+        freenode(root->right);
+        free(root);
+    }
+
+    else if(root->left != NULL && root->right == NULL)
+    {
+        freenode(root->left);
+        free(root);
+      
+    }
+
+    else if (root->left != NULL && root->right != NULL)
+    {
+      freenode(root->right);
+      freenode(root->left);
+      free(root);
+    }
+
+}
+
+
+
+
+
+
 //===========================================================================================
+// these are just to visualise the binary tree
+// https://www.geeksforgeeks.org/print-binary-tree-2-dimensions/ 
 
 
 void print2DUtil(nodeaddress root, int space)
